59d765e934573ab1d30562284365fbaf151563cf
hendisantika <hendisantika@yahoo.co.id>
2019-07-15 21:34:29 +0700

    #2 - Add procfile

--- src/main/java/com/example/demoheroku/DemoHerokuApplication.java
+++ src/main/java/com/example/demoheroku/DemoHerokuApplication.java
@@ -2,12 +2,22 @@ package com.example.demoheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @SpringBootApplication
+@RestController
 public class DemoHerokuApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoHerokuApplication.class, args);
+    }
+
+    @GetMapping("/")
+    String index() {
+        return "Hello Heroku! " + new Date();
     }
 
 }
